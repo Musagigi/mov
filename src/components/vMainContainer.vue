fa1f7de13bbb39ea824325903ccfe815bb9caf58
Musajiji <dzhal@bk.ru>
2023-05-09 21:20:42 +0300

    добавил vMainContainer

---
+++ src/components/vMainContainer.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="main">
+		<h2>{{ title }}</h2>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "vMainContainer",
+	components: {},
+	props: {},
+	data() {
+		return {
+			title: "Hello2",
+		};
+	},
+	computed: {},
+	methods: {},
+	watch: {},
+	mounted() {
+		console.log("test");
+	},
+};
+</script>
+
+<style>
+.main {
+	background-color: #ff1;
+}
+</style>
