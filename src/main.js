5ffea26a51902be56e4723b98ea925e81b52c5c7
Musajiji <dzhal@bk.ru>
2023-05-11 22:57:09 +0300

    -

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
-const pinia = createPinia()
 const app = createApp(App)
+const pinia = createPinia()
 
 app.use(pinia)
 app.mount('#app')
--- src/stores/movieStore.js
+++ src/stores/movieStore.js
@@ -1,7 +1,9 @@
 import { defineStore } from 'pinia'
+import axios from 'axios';
 
 
-export const useMovieStore = defineStore('movieStore', {
+export const useMovieStore = defineStore({
+	id: 'movieStore',
 	state: () => {
 		return {
 			count: 0,
